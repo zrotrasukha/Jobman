9f1dc785ba12ae1b445c6cb67336d8274dfa5753
zrotrasukha <zrotrasukha@gmail.com>
2026-05-06 04:25:03 +0530

    Write test for healthcheckHandler handler

---
+++ cmd/api/handlers_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/zrotrasukha/GO---Job-Application-Manager/internal/assert"
+)
+
+func TestHealth(T *testing.T) {
+	ts := newTestServer()
+
+	status, body := ts.Get(T, "/v1/healthcheck")
+
+	assert.Equal(T, status, http.StatusOK)
+	assert.Equal(T, body, "Status: OK,\nEnvironment: test")
+}
