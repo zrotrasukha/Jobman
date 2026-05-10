207566e9e9d7b152b10633a786ef81499332919f
zrotrasukha <zrotrasukha@gmail.com>
2026-05-11 00:01:42 +0530

    Refactor main application setup and database connection handling

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,34 +1,86 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 	"log/slog"
 	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/zrotrasukha/GO---Job-Application-Manager/internal/data"
 )
 
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int32
+		maxIdleConns int32
+		maxIdleTime  time.Duration
+	}
 }
 
 type application struct {
 	config config
 	logger *slog.Logger
+	models data.Models
+}
+
+func openDB(cfg config) (*pgxpool.Pool, error) {
+	poolCfg, err := pgxpool.ParseConfig(cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	poolCfg.MaxConns = cfg.db.maxOpenConns
+	poolCfg.MinConns = cfg.db.maxIdleConns
+	poolCfg.MaxConnIdleTime = cfg.db.maxIdleTime
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), poolCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err = pool.Ping(ctx); err != nil {
+		pool.Close()
+		return nil, err
+	}
+
+	return pool, nil
+
 }
 
 func main() {
 	var cfg config
+
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.env, "env", "environment", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("DSN"), "PostgreSQL DSN")
 
 	flag.Parse()
 
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	logger.Info("database connection pool established")
+
 	app := &application{
 		config: cfg,
-		logger: slog.New(slog.NewTextHandler(os.Stdout, nil)),
+		logger: logger,
+		models: data.NewModels(db),
 	}
 
-	err := app.serve()
+	err = app.serve()
 	if err != nil {
 		app.logger.Error(err.Error())
 		os.Exit(1)
