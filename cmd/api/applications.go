d84226c6e49591634c21edd5c83caa91df04217c
zrotrasukha <zrotrasukha@gmail.com>
2026-05-16 02:19:58 +0530

    Implement CreateApplicationHandler and update Application struct for better data handling

--- cmd/api/applications.go
+++ cmd/api/applications.go
@@ -3,10 +3,50 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/zrotrasukha/GO---Job-Application-Manager/internal/data"
 )
 
 func (app *application) CreateApplicationHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "add application")
+	var input struct {
+		Company_name      string `json:"company_name"`
+		RoleTitle         string `json:"role_title"`
+		Status            string `json:"status"`
+		AppliedAt         string `json:"applied_at"`
+		UpdatedAt         string `json:"updated_at"`
+		LastCommunication string `json:"last_communication"`
+		Notes             string `json:"notes"`
+	}
+
+	err := ReadJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	var application = &data.Application{
+		CompanyName:       input.Company_name,
+		RoleTitle:         input.RoleTitle,
+		Status:            input.Status,
+		AppliedAt:         input.AppliedAt,
+		UpdatedAt:         input.UpdatedAt,
+		LastCommunication: input.LastCommunication,
+		Notes:             input.Notes,
+	}
+
+	err = app.models.Application.Insert(application)
+	if err != nil {
+		app.serverErrResponse(w, r)
+		return
+	}
+
+	header := make(http.Header)
+	header.Set("Location", fmt.Sprintf("/v1/applications/%d", application.ID))
+
+	err = WriteJSON(w, http.StatusCreated, envelop{"application": application}, header)
+	if err != nil {
+		app.serverErrResponse(w, r)
+	}
 }
 
 func (app *application) GetApplicationsHandler(w http.ResponseWriter, r *http.Request) {
--- internal/data/applications.go
+++ internal/data/applications.go
@@ -1,10 +1,16 @@
 package data
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/zrotrasukha/GO---Job-Application-Manager/internal/validator"
+)
 
 type Application struct {
 	ID                int64  `json:"id"`
-	Company_name      string `json:"company_name"`
+	CompanyName       string `json:"company_name"`
 	RoleTitle         string `json:"role_title"`
 	Status            string `json:"status"`
 	AppliedAt         string `json:"applied_at"`
@@ -16,4 +22,30 @@ type Application struct {
 
 type ApplicationModel struct {
 	pool *pgxpool.Pool
+}
+
+func (m ApplicationModel) Insert(jobApp *Application) error {
+	query := `INSERT INTO applications (company_name, role_title, status, applied_at, updated_at, last_communication, notes)
+						VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`
+
+	args := []any{
+		jobApp.CompanyName,
+		jobApp.RoleTitle,
+		jobApp.Status,
+		jobApp.AppliedAt,
+		jobApp.UpdatedAt,
+		jobApp.LastCommunication,
+		jobApp.Notes,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	return m.pool.QueryRow(ctx, query, args...).Scan(&jobApp.ID)
+}
+
+func ValidateApplication(v *validator.Validator, jobApp *Application) {
+	v.CheckField(jobApp.CompanyName != "", "company_name", "must be provided")
+	v.CheckField(len(jobApp.CompanyName) <= 200, "company_name", "must not be more than 200 bytes long")
+
 }
