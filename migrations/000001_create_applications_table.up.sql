f77cd723de39eb167f7469dfa69a0ec5d02dfd34
zrotrasukha <zrotrasukha@gmail.com>
2026-05-16 20:30:40 +0530

    Fix typo in column name and adjust data types in applications table migration

--- migrations/000001_create_applications_table.up.sql
+++ migrations/000001_create_applications_table.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS applications (
     id bigserial PRIMARY KEY,
     company_name text NOT NULL,
-    orle_title text NOT NULL,
+    role_title text NOT NULL,
     status text NOT NULL,
-    created_at timestamptz NOT NULL DEFAULT now(),
+    applied_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz NOT NULL DEFAULT now(),
-    last_communication text NOT NULL,
-    notes text NOT NULL,
-    version int NOT NULL DEFAULT 1
+    last_communication timestamptz,
+    notes text,
+    version integer NOT NULL DEFAULT 1
 );
 
