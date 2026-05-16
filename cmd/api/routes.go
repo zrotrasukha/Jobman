3271c2670b58c5e13690fd0d1c2a4124082134ff
zrotrasukha <zrotrasukha@gmail.com>
2026-05-16 20:26:30 +0530

    Set up NotFound handler and  reqLogger middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,6 +8,7 @@ import (
 
 func (app *application) routes() http.Handler {
 	mux := httprouter.New()
+	mux.NotFound = http.HandlerFunc(app.notFoundResponse)
 
 	mux.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	mux.HandlerFunc(http.MethodPost, "/v1/applications", app.CreateApplicationHandler)
@@ -16,6 +17,5 @@ func (app *application) routes() http.Handler {
 	mux.HandlerFunc(http.MethodPut, "/v1/applications/:id", app.UpdateApplicationHandler)
 	mux.HandlerFunc(http.MethodDelete, "/v1/applications/:id", app.DeleteApplicationHandler)
 
-	return mux
-
+	return app.reqLogger(mux)
 }
