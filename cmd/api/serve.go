7d1ae0ecc0b587ec10ca231e673eb10f8f9217dd
zrotrasukha <zrotrasukha@gmail.com>
2026-05-17 23:06:40 +0530

    implement graceful shutdown and server timeouts

--- cmd/api/serve.go
+++ cmd/api/serve.go
@@ -1,21 +1,60 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func (app *application) serve() error {
 	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%d", app.config.port),
-		Handler: app.routes(),
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
 	}
+
+	shutdownErr := make(chan error)
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+		sig := <-sigChan
+		app.logger.Info("Interruption detected", "Signal", sig.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownErr <- err
+			app.logger.Error("Graceful shutdown failed")
+			return
+		}
+
+		app.wg.Wait()
+		shutdownErr <- nil
+	}()
 
 	app.logger.Info("Starting server", "port", app.config.port, "env", app.config.env)
 
 	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownErr
 	if err != nil {
 		return err
 	}
+
+	app.logger.Info("The server has been shut gracefully", "addr", srv.Addr)
+
 	return nil
 }
