b8b9d216e1873c3589c01f6cd2f590f355981535
zrotrasukha <zrotrasukha@gmail.com>
2026-05-17 15:38:13 +0530

    Refactor healthcheckHandler to send json response

--- cmd/api/healthcheckHandler.go
+++ cmd/api/healthcheckHandler.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	message := fmt.Sprintf("Status: OK,\nEnvironment: %s", app.config.env)
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(message))
+	message := envelop{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := WriteJSON(w, http.StatusOK, message, nil)
+	if err != nil {
+		app.serverErrResponse(w, r)
+	}
 }
