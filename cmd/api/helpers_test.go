450238cae83cb1b14cb0e99dc4426f2c57550258
zrotrasukha <zrotrasukha@gmail.com>
2026-05-19 00:50:41 +0530

    Write test for app.readJSON() helper

--- cmd/api/helpers_test.go
+++ cmd/api/helpers_test.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/zrotrasukha/jobman/internal/assert"
@@ -40,5 +41,79 @@ func TestWriteJSON(t *testing.T) {
 	assert.EqualJSON(t, b, env)
 }
 
-//TODO: Write test for readJSON
+func TestReadJSON(t *testing.T) {
+	type Input struct {
+		Name string `json:"name"`
+	}
+	tests := []struct {
+		name string
+		body string
+		err  string
+	}{
+		{
+			name: "valid body",
+			body: `{"name": "test"}`,
+			err:  "",
+		},
+		{
+			name: "empty body",
+			body: ``,
+			err:  "body must not be empty",
+		},
+		{
+			name: "malformed body",
+			body: `{"name": "test"`,
+			err:  "malformed JSON",
+		},
+		{
+			name: "invalid value for field",
+			body: `{"name": 123}`,
+			err:  "invalid value for field \"name\" (at character 12)",
+		},
+		{
+			name: "unknown field",
+			body: `{"name": "test", "age": 30}`,
+			err:  `unknown field "age"`,
+		},
+		{
+			name: "multiple JSON values in body",
+			body: `{"name": "test"}{"name": "test2"}`,
+			err:  "body must only contain a single JSON value",
+		},
+		{
+			name: "large body",
+			body: `{"name": "` + strings.Repeat("a", 1_048_577) + `"}`,
+			err:  "body must not be larger than 1048576 bytes",
+		},
+	}
+	app := newTestApplication(t)
+	ts := newTestServer(t)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+
+			var dst Input
+
+			req := httptest.NewRequest(http.MethodPost, ts.URL, strings.NewReader(tt.body))
+			err := app.readJSON(w, req, &dst)
+
+			if tt.err == "" {
+				if err != nil {
+					t.Fatalf("unexpected error: %v", err)
+				}
+			} else {
+				if err == nil {
+					t.Fatalf("got nil error; want: %v", tt.err)
+				} else {
+					if !strings.Contains(err.Error(), tt.err) {
+						t.Fatalf("got error: %v; want error: %v", err, tt.err)
+					}
+				}
+			} //t.run end
+		}) //iteration end
+	}
+}
+
+//
 //TODO: Write test for readParamID
