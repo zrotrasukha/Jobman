6d83d16830c2d4867c8a77c3b2b5e39cb0e79f92
zrotrasukha <zrotrasukha@gmail.com>
2026-05-19 03:17:34 +0530

    Refactor app.readParamID helper

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -78,12 +78,11 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 
 func (app *application) readParamID(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
-	id := params.ByName("id")
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 
-	intID, err := strconv.ParseInt(id, 10, 64)
-	if err != nil || intID < 1 {
+	if err != nil || id < 1 {
 		return 0, errors.New("invalid id parameter")
 	}
 
-	return intID, nil
+	return id, nil
 }
