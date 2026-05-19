665c4c4a1ae0151ac6adf5a185cbebd33c697e18
zrotrasukha <zrotrasukha@gmail.com>
2026-05-19 19:24:07 +0530

    Refactor TestHealthcheck and add TestCreateApplicationHandler for application creation

--- cmd/api/handlers_test.go
+++ cmd/api/handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"strings"
 	"testing"
@@ -10,8 +11,6 @@ import (
 
 func TestHealthcheck(t *testing.T) {
 	ts := newTestServer(t)
-	defer ts.Close()
-
 	app := newTestApplication(t)
 
 	expected := envelop{
@@ -25,4 +24,30 @@ func TestHealthcheck(t *testing.T) {
 
 	assert.Equal(t, status, http.StatusOK)
 	assert.EqualJSON(t, []byte(body), expected)
+}
+
+func TestCreateApplicationHandler(t *testing.T) {
+	t.Run("valid input", func(t *testing.T) {
+		ts := newTestServer(t)
+
+		input := `{
+			"company_name": "Test Company",
+			"role_title": "Test Role",
+			"status": "Applied",
+			"notes": "Test Notes"
+		}`
+
+		status, header, body := ts.Post(t, "/v1/applications", input)
+		body = strings.TrimSpace(body)
+
+		assert.Equal(t, status, http.StatusCreated)
+		assert.Equal(t, header.Get("Location"), "/v1/applications/1")
+
+		var got envelop
+		err := json.Unmarshal([]byte(body), &got)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.EqualJSON(t, []byte(body), &got)
+	})
 }
