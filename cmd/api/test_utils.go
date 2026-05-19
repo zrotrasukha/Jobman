acb26c24423bcc5dd749dfad55ae3e78d7551a97
zrotrasukha <zrotrasukha@gmail.com>
2026-05-19 19:24:18 +0530

    Enhance test utilities by adding Post method to testServer for easier POST request handling

--- cmd/api/test_utils.go
+++ cmd/api/test_utils.go
@@ -4,18 +4,21 @@ import (
 	"bytes"
 	"io"
 	"log/slog"
+	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/zrotrasukha/jobman/internal/data/mocks"
 )
 
 func newTestApplication(t *testing.T) *application {
-
 	cfg := config{
 		env: "test",
 	}
 	return &application{
 		config: cfg,
 		logger: slog.New(slog.NewTextHandler(t.Output(), nil)),
+		models: mocks.NewMockModels(),
 	}
 }
 
@@ -26,6 +29,8 @@ type testServer struct {
 func newTestServer(t *testing.T) *testServer {
 	app := newTestApplication(t)
 	ts := httptest.NewServer(app.routes())
+	t.Cleanup(ts.Close)
+
 	return &testServer{ts}
 }
 
@@ -43,4 +48,24 @@ func (ts *testServer) Get(t *testing.T, urlPath string) (int, string) {
 
 	body = bytes.TrimSpace(body)
 	return rs.StatusCode, string(body)
+}
+
+// ts.Post() will seend a POST request to the test server with the given url path and body, and return the status code, headers, and response body.
+func (ts *testServer) Post(t *testing.T, urlPath string, body string) (int, http.Header, string) {
+	url := ts.URL + urlPath
+	buf := bytes.NewBufferString(body)
+	rs, err := ts.Client().Post(url, "application/json", buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rs.Body.Close()
+
+	respBody, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	respBody = bytes.TrimSpace(respBody)
+	// return status, headers, body
+	return rs.StatusCode, rs.Header, string(respBody)
 }
