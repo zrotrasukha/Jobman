3e56da6a14dc875242fc5470893b9e722d2d8130
zrotrasukha <zrotrasukha@gmail.com>
2026-05-26 04:42:06 +0530

    Add mock db layer service functions and done minimal refactoring

--- internal/data/mocks/applications.go
+++ internal/data/mocks/applications.go
@@ -6,11 +6,14 @@ import (
 	"github.com/zrotrasukha/jobman/internal/data"
 )
 
-var fixedDate = time.Date(2026, 8, 12, 11, 45, 0, 0, time.UTC)
+var FixedDate = time.Date(2026, 8, 12, 11, 45, 0, 0, time.UTC)
 
 type MockJobApplicationModel struct {
 	InsertFunc func(jobApp *data.JobApplication) error
 	GETFunc    func(id int64) (*data.JobApplication, error)
+	GetAllFunc func(searchString string, filters data.Filters) ([]*data.JobApplication, *data.Metadata, error)
+	UpdateFunc func(jobApp *data.JobApplication) error
+	DeleteFunc func(id int64) error
 }
 
 func (m MockJobApplicationModel) Insert(jobApp *data.JobApplication) error {
@@ -19,8 +22,8 @@ func (m MockJobApplicationModel) Insert(jobApp *data.JobApplication) error {
 	}
 	jobApp.ID = 1
 	jobApp.CompanyName = "Test Company"
-	jobApp.UpdatedAt = fixedDate
-	jobApp.AppliedAt = &fixedDate
+	jobApp.UpdatedAt = FixedDate
+	jobApp.AppliedAt = &FixedDate
 	jobApp.RoleTitle = "Test Role"
 	jobApp.Status = data.StatusApplied
 	jobApp.Notes = "Test Notes"
@@ -39,12 +42,67 @@ func (m MockJobApplicationModel) Get(id int64) (*data.JobApplication, error) {
 			CompanyName: "Test Company",
 			RoleTitle:   "Test Role",
 			Status:      data.StatusApplied,
-			AppliedAt:   &fixedDate,
-			UpdatedAt:   fixedDate,
+			AppliedAt:   &FixedDate,
+			UpdatedAt:   FixedDate,
 			Notes:       "Test Notes",
 			Version:     1,
 		}, nil
 	}
 
 	return nil, data.ErrRecordNotFound
+}
+
+func (m MockJobApplicationModel) GetAll(searchString string, filters data.Filters) ([]*data.JobApplication, *data.Metadata, error) {
+	if m.GetAllFunc != nil {
+		return m.GetAllFunc(searchString, filters)
+	}
+	jobApps := []*data.JobApplication{
+		{
+			ID:          1,
+			CompanyName: "Test Company 1",
+			RoleTitle:   "Test Role 1",
+			Status:      data.StatusApplied,
+			AppliedAt:   &FixedDate,
+			UpdatedAt:   FixedDate,
+			Notes:       "Test Notes 1",
+			Version:     1,
+		},
+		{
+			ID:          2,
+			CompanyName: "Test Company 2",
+			RoleTitle:   "Test Role 2",
+			Status:      data.StatusInterviewing,
+			AppliedAt:   &FixedDate,
+			UpdatedAt:   FixedDate,
+			Notes:       "Test Notes 2",
+			Version:     1,
+		},
+	}
+	metadata := &data.Metadata{
+		CurrentPage:  1,
+		PageSize:     20,
+		FirstPage:    1,
+		LastPage:     1,
+		TotalRecords: 2,
+	}
+	return jobApps, metadata, nil
+}
+
+func (m MockJobApplicationModel) Update(jobApp *data.JobApplication) error {
+	if m.UpdateFunc != nil {
+		return m.UpdateFunc(jobApp)
+	}
+	jobApp.UpdatedAt = FixedDate
+	jobApp.Version++
+	return nil
+}
+
+func (m MockJobApplicationModel) Delete(id int64) error {
+	if m.DeleteFunc != nil {
+		return m.DeleteFunc(id)
+	}
+	if id == 1 {
+		return nil
+	}
+	return data.ErrRecordNotFound
 }
