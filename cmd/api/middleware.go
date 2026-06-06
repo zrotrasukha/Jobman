9355e1d07dd42dd65de03de2d3a0bc3ec604ef98
zrotrasukha <zrotrasukha@gmail.com>
2026-06-07 03:30:09 +0530

    Refactor middleware to improve error handling and logging structure

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,11 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // reqLogger middleware is use to log the incoming HTTP requests. It logs the HTTP method and the URL of each request when it starts processing.
 func (app *application) reqLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.logger.Info("request started", "method", r.Method, "url", r.URL.String())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverErrResponse(w, r, fmt.Errorf("panic: %v", err))
+				return
+			}
+		}()
+
 		next.ServeHTTP(w, r)
 	})
 }
