b357d14007d90c2a39e614b1273916d2e47b416a
zrotrasukha <zrotrasukha@gmail.com>
2026-06-19 20:08:42 +0530

    feat: add context management functions for user and job application retrieval

--- cmd/api/context.go
+++ cmd/api/context.go
@@ -15,3 +15,24 @@ func (app *application) ContextSetUser(r *http.Request, user *data.User) *http.R
 	ctx := context.WithValue(r.Context(), userContextKey, user)
 	return r.WithContext(ctx)
 }
+
+func (app *application) ContextGetUser(r *http.Request) *data.User {
+	user, ok := r.Context().Value(userContextKey).(*data.User)
+	if !ok {
+		panic("missing user value in request context")
+	}
+	return user
+}
+
+func (app *application) ContextSetApplication(r *http.Request, application *data.JobApplication) *http.Request {
+	ctx := context.WithValue(r.Context(), "application", application)
+	return r.WithContext(ctx)
+}
+
+func (app *application) ContextGetApplication(r *http.Request) *data.JobApplication {
+	application, ok := r.Context().Value("application").(*data.JobApplication)
+	if !ok {
+		panic("missing application value in request context")
+	}
+	return application
+}
